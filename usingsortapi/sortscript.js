ad8e606bf622e3a0bca73ea02486cfde60d1f022
sudhanshu <sudoofe123@gmail.com>
2022-03-17 21:57:54 +0530

    first commit

---
+++ usingsortapi/sortscript.js
@@ -0,0 +1,9 @@
+const columns = document.querySelectorAll(".column");
+
+columns.forEach((column) => {
+  new Sortable(column, {
+    group: "shared",
+    animation: 150,
+    ghostClass: "blue-background-class",
+  });
+});
